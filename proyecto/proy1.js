851b4035fa4a9bea090c74f39ba4a530b5bf5db2
camicorba <camincorbalan@gmail.com>
2021-10-26 23:22:28 -0300

    modifico proyecto

--- proyecto/proy1.js
+++ proyecto/proy1.js
@@ -1,30 +1,17 @@
 class Producto{
-    constructor (nombre, precio, iva, stock) {
+    constructor (nombre, precio, stock) {
         this.nombre = nombre.toLowerCase()
         this.precio = precio
-        this.iva = iva
         this.stock = stock
     }
     info () {
         document.write("Nombre del producto: " + this.nombre + "<br>")
-        document.write("Tipo de producto: " + this.tipo + "<br>")
         document.write("Precio del producto: " + this.precio + "<br>")
         document.write("Cantidad de stock " + this.stock + "<br>")
         document.write("<br>")
     }
-    valorIva() {
-        if (this.iva == 1) {
-        return this.iva=1.21;
-        }
-        else if (this.iva == 2 ){
-        return this.iva=1;
-        }
-        else {
-        throw new Error ("Debes ingresar un valor correcto: 1 / 2,")
-        }
-    }
     calculoIva () {
-    return  this.calcIva = (this.precio * this.iva);
+      this.precioIva = this.precio * 1.21
     }
 }
 
@@ -32,24 +19,27 @@ class Producto{
 // INGRESO DATOS MEDIANTE PROMPT
 const productos = [];
 do {
-    const agregar = prompt("Ingrese el nombre de un producto. Para finalizar, escribir 'esc'.");
-    if (agregar === "esc" || agregar === "Esc" || agregar === "ESC" ){
+    const agregar = (prompt("Ingrese el nombre de un producto. Para finalizar, escribir 'esc'.")).toLowerCase;
+    if (agregar === "esc"){
         break;
     } else {
         nombre = agregar;
         let precio= Number(prompt("Ingresa el precio del producto"));
-        let iva= Number(prompt("Ingresa 1 si incluye IVA. Ingresa 2 si no incluye iVA"));
         let stock = Number(prompt("Ingresa la cantidad de stock"))
-        productos.push(new Producto(nombre, precio, iva, stock));
+        productos.push(new Producto(nombre, precio, iva, precioIva, stock));
     }
-} while (nombre != "esc" || nombre != "Esc" || nombre != "ESC" );
+} while (nombre != "esc");
 
 console.log(productos)
+// for(const producto of productos){
+//     producto.calculoIva();
+// }
+
 
 for (const producto of productos){
     document.write("Nombre del producto: " + producto.nombre + "<br>")
     document.write("Precio del producto: " + producto.precio + "<br>")
-    document.write("Precio del producto con IVA: " + producto.calcIva + "<br>")
+    document.write("Precio del producto con IVA: " + producto.precioIva + "<br>")
     document.write("Cantidad de stock " + producto.stock + "<br>")
     document.write("<br>")
 }
