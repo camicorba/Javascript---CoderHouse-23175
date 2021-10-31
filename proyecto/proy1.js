9c48da7b8b088945a39fbd8d024f90b7449c167c
camila corbalan <camincorbalan@gmail.com>
2021-10-30 23:48:53 -0300

    desafio eventos + complementario

--- proyecto/proy1.js
+++ proyecto/proy1.js
@@ -1,87 +1,71 @@
 class Tarea{
-    constructor (name, desc, estado) {
-        this.name = name.toLowerCase()
+    constructor (name, desc) {
+        this.name = name
         this.desc = desc
-        this.estado = estado
-    }
-    info () {
-        document.write("Tarea: " + this.name + "<br>")
-        document.write("Descripcion: " + this.desc + "<br>")
-        document.write("Estado " + this.estado + "<br>")
-        document.write("<br>")
     }
 }
 
-// CREO LA LISTA DONDE SE GUARDAN LAS TAREAS
-let tareas = JSON.parse(localStorage.getItem('tareas')) || []
+//GUARDO EL ARRAY EN EL STORAGE
+let arraytask = JSON.parse(localStorage.getItem('arrayTask')) || []
 
 //METODO PARA AGREGAR LOS TAREAS A LA LISTA
 const create = (tarea) => {
-    tareas.push(tarea)
-    localStorage.setItem('tareas', JSON.stringify(tareas))
+    arraytask.push(tarea)
+    localStorage.setItem('arrayTask', JSON.stringify(arraytask))
 }
 
 //ACCEDO A LOS ELEMENTOS DEL DOM
 const listaTask = document.getElementById('lista-task')
 const formTask = document.getElementById('form-task')
-const inputName = document.getElementById('input-name')
+const inputName = document.getElementById('input-tarea')
 const inputDesc = document.getElementById('input-desc')
-const inputEstado = document.getElementById('input-estado')
 
 
 //AGREGO LAS TAREAS
-for (let tarea of tareas) {
+for (let tarea of arraytask) {
 
     let itemTask = document.createElement('div')
 
     itemTask.innerHTML = `
-                            Tarea: ${tarea.name} <br>
-                            Descripcion: ${tarea.desc} <br>
-                            Estado: ${tarea.estado} <br>
+                        <strong> ${tarea.name}</strong> <br>
+                        <i> ${tarea.desc}</i> <br>
                             `
 
     listaTask.appendChild(itemTask)
 }
 
+
+//EVENT PARA AGREGAR TAREAS
 formTask.addEventListener('submit', (event) => {
     const name = inputName.value
     const desc = inputDesc.value
-    const estado = inputEstado.value
 
-    const tarea = new Tarea(name, desc, estado)
+    const tarea = new Tarea(name, desc)
 
-    create(tarea)
-
-
+    create(tarea);
 })
 
-// INGRESO DATOS MEDIANTE PROMPT
 
-
-
-// const productos = [];
-// do {
-//     const agregar = (prompt("Ingrese el nombre de un producto. Para finalizar, escribir 'esc'.")).toLowerCase();
-//     if (agregar === "esc" || agregar === "Esc" || agregar === "ESC"){
-//         break;
+//EVENTO CON VALIDADOR
+// addbtn.addEventListener("click", (e)=>{
+//     e.preventDefault();
+//     let error = validar();
+//     if (error[0]) {
+//         resultado.innerHTML = error[1]
 //     } else {
-//         nombre = agregar;
-//         let precio= Number(prompt("Ingresa el precio del producto"));
-//         let stock = Number(prompt("Ingresa la cantidad de stock"))
-//         productos.push(new Producto(nombre, precio, stock));
+//         resultado.innerHTML = "Tarea agregada";
 //     }
-// } while (nombre != "esc");
+//     AgregarAlDom();
+// });
 
-// console.log(productos)
-// for(const producto of productos){
-//     producto.calculoIva();
+// const validar = () =>{
+//     let error = [];
+//     if (inputName.value.length <=1){
+//         error[0] = true
+//         error[1] = "Debe agregar una tarea";
+//         return error;
+//     }
+//     error[0] = false
+//     return error;
 // }
 
-
-// for (const producto of productos){
-//     document.write("Nombre del producto: " + producto.nombre + "<br>")
-//     document.write("Precio del producto: " + producto.precio + "<br>")
-//     document.write("Precio del producto con IVA: " + producto.precioIva + "<br>")
-//     document.write("Cantidad de stock " + producto.stock + "<br>")
-//     document.write("<br>")
-// }
